2ea9f1df62b436abeed09c31bf69401c4713787e
Omek1337 <lipskij2@gmail.com>
2020-02-13 15:24:33 +0200

    Excersises for 2nd lesson completed
    2 classes added:
    -Main class where you choode which method to use
    - Methods class which has all the logic inside

---
+++ excersises2/excersises2/Methods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersises2
+{
+    public class Methods
+    {
+        public void HelloMethod()
+        {
+            Console.WriteLine("Welcome firends!");
+            Console.WriteLine("Have a nice day!");
+        }
+        public void SpaceCount()
+        {
+            Console.WriteLine("Input any sentence you want to count spaces in");
+            string str = Console.ReadLine();
+            
+            int count = 0;
+            foreach (char c in str)
+                if (c == ' ') count++;
+            Console.WriteLine(count);
+        }
+        public void CalculateElements()
+        {
+            int[] m = new int[5];
+            Console.WriteLine("Input 5 numbers");
+
+            for (int i = 0; i < 5; i++)
+            {
+                m[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (var el in m)
+            {
+                sum += el;
+            }
+            Console.WriteLine("The sum of the elements int the output is: " + sum);
+        }
+
+        public void SwapNum()
+        {
+            Console.WriteLine("First number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Now the first number is {b} and second is {a}");
+        }
+
+        public void getFibonacci()
+        {
+            Console.WriteLine("Enter a number:");
+            int n = int.Parse(Console.ReadLine());
+            int fib = 0;
+            int n1 = 1, n2 = 1;
+            if (n > 2)
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    fib = n1 + n2;
+                    n1 = n2;
+                    n2 = fib;
+                }
+            }
+            else fib = 1;
+            Console.WriteLine($"The fibonacci of {n} is: {fib}");
+        }
+    }
+}
---
+++ excersises2/excersises2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace excersises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What program do you want to choose?(1,2,3,4,5)");
+            int n = int.Parse(Console.ReadLine());
+            Methods m = new Methods();
+            switch (n)
+            {
+                case 1:
+                    m.HelloMethod();
+                    break;
+                case 2:
+                   m. SpaceCount();
+                    break;
+                case 3:
+                    m.CalculateElements();
+                    break;
+                case 4:
+                    m.SwapNum();
+                    break;
+                case 5:
+                    m.getFibonacci();
+                    break;
+            }
+            Console.Read();
+        }
+        
+    }
+
+
+}
